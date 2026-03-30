f359c99ff2234c968238b93f536eb074994e0638
Mostafa Adel <lmaza2742000@gmail.com>
2026-03-31 00:56:06 +0200

    Add GitHub Pages deployment

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,12 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  output: "export",
+  basePath: "/DBell",
+  assetPrefix: "/DBell/",
+  images: {
+    unoptimized: true,
+  },
 };
 
 export default nextConfig;
